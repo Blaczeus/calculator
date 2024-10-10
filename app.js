c21376d3a2bddb267e253e7fd16ea6674873bff3
Anya Chidiebere <anyachidiebere029@gmail.com>
2024-10-10 03:41:58 +0100

    Refactor calculator logic: merge isClicked and findKey functions, update operator handling and input validation

--- app.js
+++ app.js
@@ -10,40 +10,31 @@ function clearInput ()
     input.value = '';
 }
 
-function isClicked ( num )
+function isClicked ( key )
 {
-    input.value += num;
-}
+    let operators = [ '+', '-', '*', '/' ];
+    let lastChar = input.value.slice( -1 );
 
-function findKey ( action )
-{
-    let mtd;
-    switch ( action )
+    if ( operators.includes( lastChar ) && operators.includes( key ) )
     {
-        case 'add':
-            mtd = ' + ';
-            break;
-        case 'minus':
-            mtd = ' - ';
-            break;
-        case 'multiply':
-            mtd = ' x ';
-            break;
-        case 'divide':
-            mtd = ' / ';
-            break;
+        return;
     }
 
-    if ( input.value )
+    let lastNumber = input.value.split( /[\+\-\*\/]/ ).pop();
+    if ( key === '.' && lastNumber.includes( '.' ) )
     {
-        input.value += mtd;
+        return;
     }
+
+    input.value += key;
 }
+
 
 function solve_equation ()
 {
     let expression = input.value;
-    clearInput();
     const result = eval( expression.replace( /[^0-9\+\-\*\/]/g, '' ) );
+    clearInput();
     input.value = result;
+    
 }
