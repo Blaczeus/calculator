3930519655efb60ff1ac36102345865d79a0812c
Anya Chidiebere <anyachidiebere029@gmail.com>
2024-10-11 01:11:45 +0100

    Added memory feature to calculator, updated key press handling, and modified display clearing and calculation result functionality.

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
 let calculatorDisplay = document.getElementById( 'calculatorDisplay' );
 let isResultDisplayed = false;
+let store = [];
 
 function deleteLastChar ()
 {
+    if ( calculatorDisplay.value.length <= 1 )
+    {
+        clearDisplay();
+    }
     calculatorDisplay.value = calculatorDisplay.value.slice( 0, -1 );
 }
 
 function clearDisplay ()
 {
     calculatorDisplay.value = '';
+    isResultDisplayed = false;
 }
 
-function handleKeyPress( key )
+function handleKeyPress ( key )
 {
     let operators = [ '+', '-', '*', '/' ];
     let lastChar = calculatorDisplay.value.slice( -1 );
 
-    if ( isResultDisplayed && !operators.includes( key ) )
+    if ( isResultDisplayed && !isNaN( key ) && !operators.includes( lastChar ) )
     {
-        clearDisplay();
-        isResultDisplayed = false;
+        return
     }
     if ( operators.includes( lastChar ) && operators.includes( key ) )
     {
@@ -35,13 +40,46 @@ function handleKeyPress( key )
     calculatorDisplay.value += key;
 }
 
+function saveResult ( result )
+{
+    store.push( result );
+    if ( store.length > 10 )
+    {
+        store.shift();
+    }
+    localStorage.setItem( 'storedResult', JSON.stringify( store ) );
+    getStoredResult();
+}
+
+function getStoredResult ()
+{
+    let storedResult = JSON.parse( localStorage.getItem( 'storedResult' ) );
+    if ( storedResult )
+    {
+        store = storedResult;
+        let result = store[ store.length - 1 ];
+        calculatorDisplay.value = result;
+        isResultDisplayed = true;
+    } else
+    {
+        clearDisplay();        
+    }
+}
+
+function clearMemory ()
+{
+    store = [];
+    localStorage.removeItem( 'storedResult' );
+    clearDisplay();
+}
+
 
 function calculateResult ()
 {
     let operators = [ '+', '-', '*', '/' ];
     let lastChar = calculatorDisplay.value.slice( -1 );
 
-    if ( operators.includes( lastChar ))
+    if ( operators.includes( lastChar ) && !isResultDisplayed )
     {
         return;
     }
@@ -50,11 +88,8 @@ function calculateResult ()
     try
     {
         const result = eval( expression.replace( /[^0-9\+\-\*\/\.]/g, '' ) );
-        clearDisplay();
-        
-        calculatorDisplay.value = result;
-
-        isResultDisplayed = true;
+        saveResult( result );
+        // isResultDisplayed = true;
     } catch ( error )
     {
         calculatorDisplay.value = "Error";
@@ -64,9 +99,9 @@ function calculateResult ()
 document.addEventListener( 'keydown', function ( event )
 {
     let key = event.key;
-    let operators = [ '+', '-', '/', '*', '.'];
+    let operators = [ '+', '-', '/', '*', '.' ];
 
-    if ( !isNaN( key ) || operators.includes(key))
+    if ( !isNaN( key ) || operators.includes( key ) )
     {
         handleKeyPress( key );
     }
@@ -81,7 +116,7 @@ document.addEventListener( 'keydown', function ( event )
         deleteLastChar();
     }
 
-    if ( key === 'Escape' || key.toLowerCase === 'c'  || key === 'Delete')
+    if ( key === 'Escape' || key.toLowerCase === 'c' || key === 'Delete' )
     {
         clearDisplay();
     }
